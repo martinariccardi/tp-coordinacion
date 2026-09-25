23ff9635469aac6459184e1b9653db9be8357b50
martinariccardi <mriccardi@fi.uba.ar>
2026-09-25 18:33:11 -0300

    fix: improve some names

--- python/src/sum/main.py
+++ python/src/sum/main.py
@@ -37,8 +37,8 @@ class SumFilter:
 
         self.amount_by_client_by_fruit = {} # {client_id: {fruit: amount}}
         self.expected_total_by_client = {} # {client_id: expected_total}
-        self.count_by_client = {} # {client_id: msg_count}
-        self.global_count = {} # {client_id: final_count}
+        self.messages_received_by_client = {} # {client_id: msg_count}
+        self.total_messaged_received_by_client = {} # {client_id: total_count}
         self.lock = threading.Lock()
 
     def _process_data(self, client_id, fruit, amount):
@@ -51,7 +51,7 @@ class SumFilter:
         if client_id in self.expected_total_by_client:
             self._notify_message_count(client_id, 1)   
         else: 
-            self.count_by_client[client_id] = self.count_by_client.get(client_id, 0) + 1
+            self.messages_received_by_client[client_id] = self.messages_received_by_client.get(client_id, 0) + 1
            
 
     def process_data_messsage(self, message, ack, nack):
@@ -84,18 +84,18 @@ class SumFilter:
             data_output_exchange.send(message_protocol.internal.serialize([client_id]))
 
         self.expected_total_by_client.pop(client_id, None)
-        self.global_count.pop(client_id, None)
-        self.count_by_client.pop(client_id, None)
+        self.total_messaged_received_by_client.pop(client_id, None)
+        self.messages_received_by_client.pop(client_id, None)
 
     def _process_control_message(self, message, ack, nack):
         msg_type, client_id, value = message_protocol.internal.deserialize_control_msg(message)
         with self.lock:
             if msg_type == COUNT_MSG_TYPE:
-                self.global_count[client_id] = self.global_count.get(client_id, 0) + value
+                self.total_messaged_received_by_client[client_id] = self.total_messaged_received_by_client.get(client_id, 0) + value
                 self._send_to_aggregation(client_id)
             elif msg_type == EOF_MSG_TYPE:
                 self.expected_total_by_client[client_id] = value
-                processed_items = self.count_by_client.get(client_id, 0)
+                processed_items = self.messages_received_by_client.get(client_id, 0)
                 if processed_items > 0:
                     self._notify_message_count(client_id, processed_items)
                 self._send_to_aggregation(client_id)
@@ -123,7 +123,7 @@ class SumFilter:
         self.control_exchange_publisher.send(message_protocol.internal.serialize_control_msg(message))
 
     def _validate_client_count(self, client_id):
-        confirmed_count = self.global_count.get(client_id,0)  
+        confirmed_count = self.total_messaged_received_by_client.get(client_id,0)  
         expected_count = self.expected_total_by_client.get(client_id,0)
         return client_id in self.expected_total_by_client and confirmed_count == expected_count
         
